8be2bde01af7d1b72723ad32b2346c27651c210c
Alex Sokolov <Gelezako@users.noreply.github.com>
2018-05-30 22:11:05 +0300

    Update Yeelight_set_bright.php

--- modules/Yeelight/Yeelight_set_bright.php
+++ modules/Yeelight/Yeelight_set_bright.php
@@ -1,4 +1,4 @@
-<?
+<?php
 //========= метод set_bright (установка яркости) ====================
 include_once(DIR_MODULES.'Yeelight/Yeelight_library.php');
 $Location = $this->getProperty('Location');
